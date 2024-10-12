230ca55bf4d6dea1259429f584d40f35ebcd4155
Zakary <zbbamber@gmail.com>
2024-10-12 14:12:05 +0100

    layout looking nice

--- main.py
+++ main.py
@@ -7,45 +7,50 @@ def stretchImage(event):
     global resizedMap
     width = event.width
     height = event.height
+
+    print(f'width: {width}, height: {height}')
     resizedMap = ImageTk.PhotoImage(mapImage.resize((width,height)))
     mapCanvas.create_image(0,0, image = resizedMap, anchor='nw')
+    
 
 
 app = tk.Tk()
 app.title('Safe Way Out')
 app.geometry('1280x720')
 app.columnconfigure(0, weight=1)
-app.columnconfigure(1, weight=3)
+app.columnconfigure(1, weight=20)
 app.rowconfigure(0, weight=1)
- 
-mapImage = Image.open('exampleSitePlan.png')
+app.configure(bg='white')
+app.iconbitmap('fire.ico')
 
-menuFrame = tk.Frame(app, bg='red')
-contentFrame = tk.Frame(app, bg='blue')
-contentFrame.rowconfigure(0, weight=2)
+mapImage = Image.open('siteplan.png')
+
+menuFrame = ctk.CTkFrame(app, corner_radius=15, border_color='black', border_width=5, bg_color='white', fg_color='white')
+contentFrame = ctk.CTkFrame(app, bg_color='white', fg_color='white')
+contentFrame.rowconfigure(0, weight=4)
 contentFrame.rowconfigure(1, weight=1)
 contentFrame.columnconfigure(0, weight=1)
-upperContentFrame = tk.Frame(contentFrame, bg='yellow')
+
+upperContentFrame = ctk.CTkFrame(contentFrame, bg_color='white', fg_color='white')
 upperContentFrame.columnconfigure(0, weight=5)
 upperContentFrame.columnconfigure(1, weight=2)
 upperContentFrame.rowconfigure(0, weight=1)
-lowerContentFrame = tk.Frame(contentFrame, bg='green')
-mapContainer = tk.Frame(upperContentFrame, bg='orange')
-mapCanvas = tk.Canvas(mapContainer, background='grey', bd=0, highlightthickness=0, relief='ridge')
-mapCanvas.pack(fill='both', expand=True)
+lowerContentFrame = ctk.CTkFrame(contentFrame, corner_radius=15, border_color='black', border_width=5, bg_color='white', fg_color='white')
 
-toDoContainer = tk.Frame(upperContentFrame, bg='lightblue')
+mapContainer = ctk.CTkFrame(upperContentFrame, corner_radius=15, border_color='black', border_width=5, bg_color='white', fg_color='white')
+mapCanvas = tk.Canvas(mapContainer, background='white', bd=0, highlightthickness=0, relief='ridge')
+mapCanvas.pack(fill='both', expand=True, padx=10, pady=10)
+toDoContainer = ctk.CTkFrame(upperContentFrame, corner_radius=15, border_color='black', border_width=5, bg_color='white', fg_color='white')
 
 mapContainer.grid(row=0, column=0, sticky='nsew', padx=(10, 5), pady=10)
 toDoContainer.grid(row=0, column=1, sticky='nsew', padx=(5, 10), pady=10)
 
-upperContentFrame.grid(row=0, column=0, sticky='nsew', padx=10, pady=(10, 5))
+upperContentFrame.grid(row=0, column=0, sticky='nsew')
 lowerContentFrame.grid(row=1, column=0, sticky='nsew', padx=10, pady=(5, 10))
 
-menuFrame.grid(row=0, column=0, sticky='nsew')
+menuFrame.grid(row=0, column=0, sticky='nsew', padx=(10,5), pady=10)
 contentFrame.grid(row=0, column=1, sticky='nsew')
 
 mapCanvas.bind('<Configure>', stretchImage)
-
 
 app.mainloop()
