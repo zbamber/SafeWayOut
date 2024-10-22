5cb0bdf05f628d1012fd1eb84b8871007de83235
zbamber <zbbamber@gmail.com>
2024-10-22 12:07:11 +0100

    finished the menu

--- classy.py
+++ classy.py
@@ -4,15 +4,6 @@ import customtkinter as ctk
 from PIL import Image, ImageTk
 
 class App(tk.Tk):
-    
-    menuButtonStyling = {
-    'border_width':2,
-    'border_color':'black',
-    'text_color':'black',
-    'font':('Excalifont',20),
-    'fg_color':'white',
-    'corner_radius':10
-    }
 
     checkboxStyling = {
     'width':5,
@@ -36,15 +27,44 @@ class App(tk.Tk):
         self.menu = Menu(self)
     
 class Menu(ctk.CTkFrame):
+
     def __init__(self, parent):
         super().__init__(parent)
         self.configure(corner_radius=15, border_color='black', border_width=5, bg_color='white', fg_color='white')
+        self.homeDark = ImageTk.PhotoImage(Image.open('homeDark.png'))
+        self.homeLight = ImageTk.PhotoImage(Image.open('homeLight.png'))
+        self.createMenuWidgets()
+        self.placeMenuWidgets()
         self.grid(row=0, column=0, sticky='nsew', padx=(10,5), pady=10)
     
-    def placeMenuWidgets():
-        title = ctk.CTkLabel(menuFrame, text='Safe Way Out', fg_color='white', text_color='black', font=('Excalifont', 20))
-        homeButton = ctk.CTkButton(menuFrame, text='   Home', image=homeDark, anchor='w', **menuButtonStyling)
-        optimisePlanButton = ctk.CTkButton(menuFrame, text='Optimise Plan', **menuButtonStyling)
-        inputDataButton = ctk.CTkButton(menuFrame, text='Input Data', **menuButtonStyling)
+    def createMenuWidgets(self):
+
+        menuButtonStyling = {
+        'border_width':2,
+        'border_color':'black',
+        'text_color':'black',
+        'font':('Excalifont',20),
+        'fg_color':'white',
+        'corner_radius':10
+        }
+
+        self.title = ctk.CTkLabel(self, text='Safe Way Out', fg_color='white', text_color='black', font=('Excalifont', 20))
+        self.homeButton = ctk.CTkButton(self, text='   Home', image=self.homeDark, anchor='w', **menuButtonStyling)
+        self.optimisePlanButton = ctk.CTkButton(self, text='Optimise Plan', **menuButtonStyling)
+        self.inputDataButton = ctk.CTkButton(self, text='Input Data', **menuButtonStyling)
+    
+    def placeMenuWidgets(self):
+        self.title.pack(pady=20, padx=20)
+        self.homeButton.pack(fill='x', padx=10, pady=5)
+        self.homeButton.bind('<Enter>', lambda event: self.homeButton.configure(text_color='white', fg_color='black', image=self.homeLight))
+        self.homeButton.bind('<Leave>', lambda event: self.homeButton.configure(text_color='black', fg_color='white', image=self.homeDark))
+        self.placeMenuButton(self.optimisePlanButton)
+        self.placeMenuButton(self.inputDataButton)
+
+    def placeMenuButton(self, button):
+        button.pack(fill='x', padx=10, pady=5)
+        button.bind('<Enter>', lambda event: button.configure(text_color='white', fg_color='black'))
+        button.bind('<Leave>', lambda event: button.configure(text_color='black', fg_color='white'))
+
 app = App()
 app.mainloop()
